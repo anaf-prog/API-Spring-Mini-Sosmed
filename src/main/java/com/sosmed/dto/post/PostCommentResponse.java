bb5c8b3e91208f951a55bb62382c26b8c465a872
anaf-prog <izzatannafs6@gmail.com>
2026-05-28 17:51:04 +0700

    Update DTO PostCommentResponse penambahan field image dan imageId

--- src/main/java/com/sosmed/dto/post/PostCommentResponse.java
+++ src/main/java/com/sosmed/dto/post/PostCommentResponse.java
@@ -2,6 +2,8 @@ package com.sosmed.dto.post;
 
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +13,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostCommentResponse {
 
     private Long id;
     private String content;
+    private String image;
+    private String imageId;
     private ZonedDateTime createdAt;
     private ZonedDateTime updatedAt;
     private UserPostResponse user;
