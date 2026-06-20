7dc2c05edaa57dec30533ef6980f5b6f3d46c591
anaf-prog <izzatannafs6@gmail.com>
2026-06-20 19:30:43 +0700

    Update security config untuk penambahan path error agar tidak ter blokir oleh spring security

--- src/main/java/com/sosmed/security/SecurityConfig.java
+++ src/main/java/com/sosmed/security/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/error").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
